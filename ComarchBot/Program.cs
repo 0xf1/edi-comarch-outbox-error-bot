9490e41355420cec3006a70ede6c52a48684995c
dadahanov-mr <dadakhanov@gmail.com>
2021-09-05 09:43:52 +0300

    refactoring

--- ComarchBot/Program.cs
+++ ComarchBot/Program.cs
@@ -11,36 +11,28 @@ using System.Threading.Tasks;
 namespace ComarchBot
 {
     class Program
-    {
-        static HttpClient client;
-
+    {        
         static async Task Main(string[] args)
-        {
-            
+        {            
             var baseAddress = new Uri("https://ecodweb.comarch.ru");
             var cookieContainer = new CookieContainer();
             var handler = new HttpClientHandler() { CookieContainer = cookieContainer };
-            client = new HttpClient(handler) { BaseAddress = baseAddress};
+            var client = new HttpClient(handler) { BaseAddress = baseAddress};
             client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36");
             
             // 1. First request to get aspx <input> values
-            string urlLogin = "/App/Pages/Login.aspx?info=sessionExpired&ReturnUrl=%2fApp%2fPages%2fOutbox.aspx";
+            var urlLogin = "/App/Pages/Login.aspx?info=sessionExpired&ReturnUrl=%2fApp%2fPages%2fOutbox.aspx";
             var response = await client.GetAsync(urlLogin);
+            response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
 
             // 2. Login request
-            string login;
-            using (var f = File.OpenText("_Login.txt")) {login = f.ReadLine();}
-            string password;
-            using (var f = File.OpenText("_Password.txt")) {password = f.ReadLine(); }
-
             var values = new Dictionary<string, string>
             {
-                { "__VIEWSTATE", GetInputValue("__VIEWSTATE", responseString)},
-                { "__VIEWSTATEGENERATOR", GetInputValue("__VIEWSTATEGENERATOR", responseString)},
+                { "__VIEWSTATE", GetInputValue("__VIEWSTATE", responseString)},              
                 { "__EVENTVALIDATION", GetInputValue("__EVENTVALIDATION", responseString)},
-                { "MainLogin$UserName", login },
-                { "MainLogin$Password", password },
+                { "MainLogin$UserName", ReadLineFromFile("_Login.txt") },
+                { "MainLogin$Password", ReadLineFromFile("_Password.txt") },
                 { "MainLogin$CtrlLoginButton", "Вход" }
             };
             var content = new FormUrlEncodedContent(values);
@@ -48,32 +40,34 @@ namespace ComarchBot
             response.EnsureSuccessStatusCode();
 
             // 3. Error outbox request 
-            string outboxFilters = "SrcPart_Name=0&DestPart_Name=0&BusinessType_Type=0&Relation=0&DocumentNumber=&DateFrom=&DateTo=&DateDocumentFrom=&DateDocumentTo=&PrintedDocuments=Blank&CanceledDocuments=Blank&FinalizedDocuments=Blank&Error=Checked&DeliveryPointIln=&MessageType=&DocumentReferenceNumber=";
-            cookieContainer.Add(baseAddress, new Cookie("OutboxFilterParam", outboxFilters));
+            cookieContainer.Add(baseAddress, new Cookie("OutboxFilterParam", "Error=Checked"));
             cookieContainer.Add(baseAddress, new Cookie("OutboxPageSize", "50"));
-            var urlOutbox = "/App/Pages/Outbox.aspx";
-            response = await client.GetAsync(urlOutbox);
+            response = await client.GetAsync("/App/Pages/Outbox.aspx");
             response.EnsureSuccessStatusCode();
             responseString = await response.Content.ReadAsStringAsync();            
 
-            // Parse Error Docs Response
+            // 4. Parse Error Docs Response
             var errDocPattern = "<div id=\".*?DocumentNumber\".*?>(.*?)<\\/div>.*?<div id=\".*?WebIntOut_TimeStamp\" .*?>(.*?)<\\/div>";
-            Regex regex = new Regex(errDocPattern);
-            var matches = regex.Matches(responseString);
-            foreach (Match match in matches)
+            var regex = new Regex(errDocPattern);
+            foreach (Match match in regex.Matches(responseString))
             {
-                var docNo = match.Groups[1].Value;
-                var processDate = match.Groups[2].Value;
-                Console.WriteLine($"{docNo} {processDate}");
+                Console.WriteLine($"docNo: {match.Groups[1].Value}, processDate: {match.Groups[2].Value}");
             }
             
             Console.ReadLine();
+        }
 
-        }        
+        private static string ReadLineFromFile(string fileName)
+        {
+            using (var f = File.OpenText(fileName)) 
+            {
+                return f.ReadLine(); 
+            }
+        }
 
         private static string GetInputValue(string inputName, string responseBody)
         {
-            Regex regex = new Regex($"<input type=\"hidden\" name=\"{inputName}\".*?value=\"(.*?)\"");
+            var regex = new Regex($"<input type=\"hidden\" name=\"{inputName}\".*?value=\"(.*?)\"");
             return regex.Match(responseBody).Groups[1].Value;
         }
     }
